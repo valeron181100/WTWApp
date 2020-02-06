b4b3183d5dcad3ec1e7c15a58f65b869dc8218bf
valeron181100 <bondar2501@outlook.com>
2020-02-06 21:48:47 +0300

    some changes with activity resizing add password hiding

--- app/src/main/java/com/valeron/wtwapp/SplashActivity.java
+++ app/src/main/java/com/valeron/wtwapp/SplashActivity.java
@@ -8,12 +8,14 @@ import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.transition.ChangeBounds;
 import android.transition.Transition;
 import android.transition.TransitionManager;
 import android.view.ViewGroup;
 import android.view.animation.AnticipateInterpolator;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import java.util.Timer;
@@ -23,6 +25,7 @@ public class SplashActivity extends AppCompatActivity {
 
     ImageView logoIV;
     ConstraintLayout mainCL;
+    EditText mPasswordET;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +33,8 @@ public class SplashActivity extends AppCompatActivity {
 
         logoIV = findViewById(R.id.logoIV);
         mainCL = findViewById(R.id.splashConstraintLayout);
+        mPasswordET = findViewById(R.id.passwordET);
+        mPasswordET.setTransformationMethod(PasswordTransformationMethod.getInstance());
 
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
@@ -39,7 +44,7 @@ public class SplashActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         ConstraintSet constraintSet = new ConstraintSet();
-                        constraintSet.clone(SplashActivity.this, R.layout.activity_splash_anim_0);
+                        constraintSet.clone(SplashActivity.this, R.layout.activity_splash_anim_1);
                         constraintSet.applyTo(mainCL);
                         ChangeBounds transition = new ChangeBounds();
                         transition.setInterpolator(new AnticipateInterpolator(1.0f));
@@ -53,43 +58,11 @@ public class SplashActivity extends AppCompatActivity {
                             @Override
                             public void onTransitionEnd(Transition transition) {
                                 ConstraintSet constraintSet = new ConstraintSet();
-                                constraintSet.clone(SplashActivity.this, R.layout.activity_splash_anim_1);
+                                constraintSet.clone(SplashActivity.this, R.layout.activity_splash_anim_2);
                                 constraintSet.applyTo(mainCL);
                                 transition = new ChangeBounds();
                                 transition.setInterpolator(new AnticipateInterpolator(1.0f));
                                 transition.setDuration(300);
-                                transition.addListener(new Transition.TransitionListener() {
-                                    @Override
-                                    public void onTransitionStart(Transition transition) {
-
-                                    }
-
-                                    @Override
-                                    public void onTransitionEnd(Transition transition) {
-                                        ConstraintSet constraintSet = new ConstraintSet();
-                                        constraintSet.clone(SplashActivity.this, R.layout.activity_splash_anim_2);
-                                        constraintSet.applyTo(mainCL);
-                                        transition = new ChangeBounds();
-                                        transition.setInterpolator(new AnticipateInterpolator(1.0f));
-                                        transition.setDuration(300);
-                                        TransitionManager.beginDelayedTransition(mainCL, transition);
-                                    }
-
-                                    @Override
-                                    public void onTransitionCancel(Transition transition) {
-
-                                    }
-
-                                    @Override
-                                    public void onTransitionPause(Transition transition) {
-
-                                    }
-
-                                    @Override
-                                    public void onTransitionResume(Transition transition) {
-
-                                    }
-                                });
                                 TransitionManager.beginDelayedTransition(mainCL, transition);
                             }
 
@@ -113,6 +86,8 @@ public class SplashActivity extends AppCompatActivity {
                     }
                 });
             }
-        }, 1000);
+        }, 500);
+
+
     }
 }
